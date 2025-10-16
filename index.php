e5b1090e45d48cda11c85df8376f3ddaeb2ca6a7
Darlington <59008938+fritzdultimate@users.noreply.github.com>
2025-10-16 17:49:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
 | we will load this file so that any pre-rendered content can be shown
